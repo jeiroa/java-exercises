30e49461fcf3ab82ed3c22d44c763febe45e9800
Jesus Eiroa <jeiroa@gmail.com>
2024-09-12 20:22:45 +0200

    polish unit test

--- palindrome/src/test/java/es/jeiroa/exercises/palindrome/PalindromeTest.java
+++ palindrome/src/test/java/es/jeiroa/exercises/palindrome/PalindromeTest.java
@@ -10,20 +10,20 @@ class PalindromeTest {
 
 	@Test
 	void testPalindrome1() {
-		assertEquals(true, unit.isPalindrome1("racecar"));
-		assertEquals(true, unit.isPalindrome1("a"));
-		assertEquals(true, unit.isPalindrome1("aa"));
-		assertEquals(false, unit.isPalindrome1("ab"));
+		assertTrue(unit.isPalindrome1("racecar"));
+		assertTrue(unit.isPalindrome1("a"));
+		assertTrue(unit.isPalindrome1("aa"));
+		assertFalse(unit.isPalindrome1("ab"));
 		
 		assertThrows(NullPointerException.class, () -> unit.isPalindrome1(null));
 	}
 	
 	@Test
 	void testPalindrome2() {
-		assertEquals(true, unit.isPalindrome2("racecar"));
-		assertEquals(true, unit.isPalindrome2("a"));
-		assertEquals(true, unit.isPalindrome2("aa"));
-		assertEquals(false, unit.isPalindrome2("ab"));
+		assertTrue(unit.isPalindrome2("racecar"));
+		assertTrue(unit.isPalindrome2("a"));
+		assertTrue(unit.isPalindrome2("aa"));
+		assertFalse(unit.isPalindrome2("ab"));
 		
 		assertThrows(NullPointerException.class, () -> unit.isPalindrome2(null));
 	}
